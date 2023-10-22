2f81f808aa8bf8692ade318bbf049853890f1761
Carlo Mantovani <105457981+Carlo-Mantovani@users.noreply.github.com>
2023-10-22 19:28:45 +0000

    fix: fifo -> noop

--- labsisop-grp-03/modules/sstf-iosched/plot.py
+++ labsisop-grp-03/modules/sstf-iosched/plot.py
@@ -5,39 +5,39 @@ y_fifo = []
 x_sstf = []
 y_sstf = []
 
-order_fifo = 1
+order_noop = 1
 order_sstf = 1
-last_sector_fifo = None
+last_sector_noop = None
 last_sector_sstf = None
 fifo_sectors = 0
 sstf_sectors = 0
-end_fifo = False
+end_noop = False
 end_sstf = False
 
 # Read the result.txt file and store the data in the x and y arrays for each algorithm
-with open("result2.txt") as file_in:
+with open("result.txt") as file_in:
     lines = []
     # Read the file line by line
     for line in file_in:
         # If the sector limit is reached, stop reading the file
-        if end_fifo and end_sstf:
+        if end_noop and end_sstf:
             break
         # Separates the sector limits for each algorithm
-        elif order_fifo > 1000:
-            end_fifo = True
+        elif order_noop > 1000:
+            end_noop = True
             if order_sstf > 1000:
                 end_sstf = True
         # Get the sector number
         sector = int(line.split()[3])
         # Treats data for FIFO
-        if "add" in line and not end_fifo:
+        if "add" in line and not end_noop:
             x_fifo.append(sector)
-            y_fifo.append(order_fifo)
-            # Calculates the total sectors traversed by FIFO
-            if last_sector_fifo is not None:
-                fifo_sectors += abs(sector - last_sector_fifo)
-            last_sector_fifo = sector
-            order_fifo += 1
+            y_fifo.append(order_noop)
+            # Calculates the total sectors traversed by NOOP
+            if last_sector_noop is not None:
+                fifo_sectors += abs(sector - last_sector_noop)
+            last_sector_noop = sector
+            order_noop += 1
         # Treats data for SSTF
         if "dsp" in line and not end_sstf:
             x_sstf.append(sector)
@@ -53,16 +53,16 @@ with open("result2.txt") as file_in:
 # Plot the data
 plt.clf()
 plt.figure(figsize=(5, 15))
-plt.plot(x_fifo, y_fifo, label="FIFO", color="mediumslateblue")
+plt.plot(x_fifo, y_fifo, label="NOOP", color="mediumslateblue")
 plt.plot(x_sstf, y_sstf, label="SSTF", color="red")
 plt.legend()
 plt.xlabel("Sector")
 plt.ylabel("Order")
-plt.title("SSTF vs FIFO")
-plt.savefig("plot2.png")
+plt.title("SSTF vs NOOP")
+plt.savefig("plot.png")
 
 
 # Print the total sectors traversed for FIFO
-print("Total sectors traversed by FIFO:", fifo_sectors)
+print("Total sectors traversed by NOOP:", fifo_sectors)
 # Print the total sectors traversed for SSTF
 print("Total sectors traversed by SSTF:", sstf_sectors)
