dc2b8e8d85c394b4cd6e90f44cc621ff73243ae8
Carlo <c.mantovani@edu.pucrs.br>
2023-11-12 22:46:43 -0300

    fix: formatting

--- labsisop-grp-03/custom-scripts/t3/sched_profiler.c
+++ labsisop-grp-03/custom-scripts/t3/sched_profiler.c
@@ -7,17 +7,13 @@
 #include <linux/sched.h>
 #include <semaphore.h>
 
-
-
-
-
 // Buffer
-char* buffer;
+char *buffer;
 int bufferIndex = 0;
 int bufferSize;
 
 // Thread array
-pthread_t* threads;
+pthread_t *threads;
 
 // Mutex (Semaphore)
 pthread_mutex_t thread_lock;
@@ -26,8 +22,9 @@ pthread_mutex_t thread_lock;
 pthread_barrier_t thread_barrier;
 
 // Print buffer
-void print_buffer(){
-	for(int i = 0; i < bufferSize; i++){
+void print_buffer()
+{
+	for (int i = 0; i < bufferSize; i++) {
 		printf("%c", buffer[i]);
 	}
 }
@@ -35,13 +32,13 @@ void print_buffer(){
 // Thread task
 void *thread_run(void *id)
 {
-	char tag = (char)id + 0x41;// convert int to char (A, B, C, ...)
+	char tag = (char)id + 0x41; // convert int to char (A, B, C, ...)
 
 	// barrier to sync threads
 	pthread_barrier_wait(&thread_barrier);
 
 	// write to buffer until it is full
-	while(bufferIndex < bufferSize){
+	while (bufferIndex < bufferSize) {
 		pthread_mutex_lock(&thread_lock);
 		buffer[bufferIndex] = tag;
 		bufferIndex++;
@@ -51,14 +48,10 @@ void *thread_run(void *id)
 	return NULL;
 }
 
-
-
-
 int main(int argc, char **argv)
 {
-
 	// check parameters
-	if (argc <= 2){
+	if (argc <= 2) {
 		printf("Parameters: <Thread Number> <Buffer Size(mb)> \n\n");
 		return 0;
 	}
@@ -70,45 +63,43 @@ int main(int argc, char **argv)
 	pthread_barrier_init(&thread_barrier, NULL, atoi(argv[1]));
 
 	// buffer init
-	bufferSize = atoi(argv[2]) * 1000000;// convert mb to bytes
-	buffer = (char *) malloc(bufferSize);// allocate buffer memory
-
+	bufferSize = atoi(argv[2]) * 1000000; // convert mb to bytes
+	buffer = (char *)malloc(bufferSize); // allocate buffer memory
 
 	// threads array init
 	int nThreads = atoi(argv[1]);
-	threads = (pthread_t *) malloc(nThreads * sizeof(pthread_t));
+	threads = (pthread_t *)malloc(nThreads * sizeof(pthread_t));
 
 	// create threads
-	for(int i = 0; i < nThreads; i++){
-			pthread_create(&threads[i], NULL, thread_run, (void *)i);
+	for (int i = 0; i < nThreads; i++) {
+		pthread_create(&threads[i], NULL, thread_run, (void *)i);
 	}
 
-    printf("\nThreads created\n");
+	printf("\nThreads created\n");
 
 	printf("Waiting for threads to finish...\n\n");
-	for(int i = 0; i < nThreads; i++){
-			pthread_join(threads[i], NULL);
+	for (int i = 0; i < nThreads; i++) {
+		pthread_join(threads[i], NULL);
 	}
 
 	//print_buffer();
 
-	int* counter = (int*) malloc(nThreads * sizeof(int)); // array to count the number of times each thread was scheduled
+	int *counter = (int *)malloc(nThreads * sizeof(int)); // array to count the number of times each thread was scheduled
 
 	printf("Scheduling order: ");
-	for(int i = 0; i < bufferSize; i++){
-		if((i > 0 && buffer[i] != buffer[i-1]) || i==0){// if the char is different from the previous one	
+	for (int i = 0; i < bufferSize; i++) {
+		if ((i > 0 && buffer[i] != buffer[i - 1]) || i == 0) { // if the char is different from the previous one
 			printf("%c", buffer[i]); // print the char
-			counter[(int)buffer[i] - 0x41] +=  1; // increment the counter
+			counter[(int)buffer[i] - 0x41] += 1; // increment the counter
 		}
 	}
-	
+
 	printf("\n\nCounter of each thread: \n");
-	for(int i = 0; i < nThreads; i++){
-		printf("%c = %d\n", i+0x41 ,counter[i]);
+	for (int i = 0; i < nThreads; i++) {
+		printf("%c = %d\n", i + 0x41, counter[i]);
 	}
 
-
-    pthread_barrier_destroy(&thread_barrier);
+	pthread_barrier_destroy(&thread_barrier);
 	pthread_mutex_destroy(&thread_lock);
 
 	return 0;
